e92f1e91a90d14ee8445315b2ba5a96a7cb28d8e
★ W O G G E R !!! <97348720+San-77x@users.noreply.github.com>
2023-12-13 23:57:33 +0530

    Update login page styling and remove unnecessary script (generated by blackbox.ai )

--- login/script.js
+++ login/script.js
@@ -1,22 +1,11 @@
-// This script is optional, but it shows how to validate the username and password before submitting the form.
-
-const form = document.querySelector('form');
-
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const
- 
-username = document.getElementById('username').value;
-
-    
-const password = document.getElementById('password').value;
-
-    if (username !== 'admin' || password !== 'Pass') {
-        alert('Invalid username or password.');
-        return;
-    }
-
-    // Submit the form if the username and password are valid
-    form.submit();
-});
+$(".user").focusin(function(){
+    $(".inputUserIcon").css("color", "#e74c3c");
+  }).focusout(function(){
+    $(".inputUserIcon").css("color", "white");
+  });
+  
+  $(".pass").focusin(function(){
+    $(".inputPassIcon").css("color", "#e74c3c");
+  }).focusout(function(){
+    $(".inputPassIcon").css("color", "white");
+  });
